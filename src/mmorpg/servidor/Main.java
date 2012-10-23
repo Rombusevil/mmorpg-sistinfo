e85dcefe8530f0695fd6e761cf8cba359f8133d1
luismunoz.dh@gmail.com <luismunoz.dh@gmail.com@0634dfc2-1731-0b68-b3c3-2be22b4b793a>
2012-10-23 01:13:14 +0000

    Comentarios en clase. Hay que cambiar: -El imprimidor de mundos -Cambiar el codigo de los moveedores. -Agregar dibujos en los actores. -Agregar interfaz de Controlador jugadores para cuando llegue la etapa del cliente.

--- src/mmorpg/servidor/Main.java
+++ src/mmorpg/servidor/Main.java
@@ -1,45 +1,73 @@
 package mmorpg.servidor;
 
-/** Los objetos creados en los métodos son locales a los métodos? no creo, están en el heap **/
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
-public class Main {	
+/**
+ * Los objetos creados en los métodos son locales a los métodos? no creo,
+ * están en el heap
+ **/
+
+public class Main {
 	public static void main(String[] args) {
 		int alto, ancho;
 		Mundo mundo;
 		Celda entradaMundo;
 		Actor actor = new PJ();
 		Actor actor2 = new PJ();
+		List<MovedorEnDireccion> listaDeMoverdores = new ArrayList<MovedorEnDireccion>();
 
-		
-		/**Movedores**/
+		/** Movedores **/
 		MovedorDerecha movedorDer = new MovedorDerecha();
 		MovedorIzquierda movedorIz = new MovedorIzquierda();
 		MovedorArriba movedorArr = new MovedorArriba();
 		MovedorAbajo movedorAbj = new MovedorAbajo();
-			
-		/** Creo el mundo**/
-		alto = ancho = 3;	//Dimensión del mundo
-		mundo = new ImpMundo(alto,ancho);
+
+		/** Creo el mundo **/
+		alto = ancho = 3; // Dimensión del mundo
+		mundo = new ImpMundo(alto, ancho);
 		entradaMundo = mundo.getEntradaMundo();
-		
-		/**Inicializo a los actores**/
-		mundo.poneActorEn(0, 1, actor); // Pone al actor en el mundo para poder moverlo
-		mundo.poneActorEn(0,0, actor2);
-		
-		
+
+		/** Inicializo a los actores **/
+		mundo.poneActorEn(0, 1, actor); // Pone al actor en el mundo para poder
+										// moverlo
+		mundo.poneActorEn(0, 0, actor2);
+
 		mundo.muestraMundo(alto, ancho, entradaMundo);
-		
-		
-		movedorDer.mover(actor);		
-		movedorAbj.mover(actor);
-		movedorIz.mover(actor);
-		movedorAbj.mover(actor);
-		movedorIz.mover(actor);
-		movedorArr.mover(actor);
-		
-		
-		mundo.muestraMundo(alto, ancho, entradaMundo);
-		
+
+		listaDeMoverdores.add(movedorDer);
+		listaDeMoverdores.add(movedorAbj);
+		listaDeMoverdores.add(movedorIz);
+		listaDeMoverdores.add(movedorAbj);
+		listaDeMoverdores.add(movedorIz);
+		listaDeMoverdores.add(movedorArr);
+		listaDeMoverdores.add(movedorArr);
+		listaDeMoverdores.add(movedorArr);
+		listaDeMoverdores.add(movedorArr);
+		listaDeMoverdores.add(movedorDer);
+		listaDeMoverdores.add(movedorDer);
+		listaDeMoverdores.add(movedorDer);
+		listaDeMoverdores.add(movedorDer);
+		listaDeMoverdores.add(movedorDer);
+		listaDeMoverdores.add(movedorDer);
+		listaDeMoverdores.add(movedorDer);
+
+		Iterator<MovedorEnDireccion> iterator = listaDeMoverdores.iterator();
+		while (iterator.hasNext()) {
+
+			MovedorEnDireccion cadaMovedor = (MovedorEnDireccion) iterator.next();
+			cadaMovedor.mover(actor);
+			mundo.muestraMundo(alto, ancho, entradaMundo);
+		}
+
+		// movedorDer.mover(actor);
+		// movedorAbj.mover(actor);
+		// movedorIz.mover(actor);
+		// movedorAbj.mover(actor);
+		// movedorIz.mover(actor);
+		// movedorArr.mover(actor);
+
 
 	}
 }
--- src/mmorpg/servidor/MovedorDerecha.java
+++ src/mmorpg/servidor/MovedorDerecha.java
@@ -1,8 +1,9 @@
 package mmorpg.servidor;
-
+// Estos moveedores nos permiten crear patrones de movimiento para los NPJ.
 public class MovedorDerecha implements MovedorEnDireccion {
 	
 	public void mover(Actor actor){
+		//actor.moveteDer();
 		Celda celdaActual, celdaDestino;
 		
 		celdaActual = actor.getCeldaActual();
