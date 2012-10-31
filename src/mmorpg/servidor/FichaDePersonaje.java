d7aadac891b17fe708ff00735070f16db2a0eeb6
franco.mgr <franco.mgr@0634dfc2-1731-0b68-b3c3-2be22b4b793a>
2012-10-31 18:49:02 +0000

    Interfaz entre ImpActor e ImpFichaDePersonaje.
    Mediante esta interfaz, el Actor interactúa con la FichaDePersonaje. Quizá habría que agregar algunos métodos... como por ejemplo "ganeXp(int xp)", para que, cuando se gane un combate el jugador aumente su Xp.
    
    Por otro lado hay que terminar de implementar la muerte del Pj, ya que si su Hp llega a cero debe avisar que murió.

--- src/mmorpg/servidor/FichaDePersonaje.java
+++ src/mmorpg/servidor/FichaDePersonaje.java
@@ -1,280 +1,59 @@
 package mmorpg.servidor;
 
-import java.util.List; //Manejo de listas
-
-/*
- * TODO: FichaDePersonaje
- * 		 Testing de que los calculos esten bien! <-- Se necesita crear la clase Skill
- * 		 Se necesita crear la clase de los items para implementar Gear * 
- * */
-
-public class FichaDePersonaje {
-
-	/* Items Equipados del personaje -> GEAR */
-	Gear gear;
-
-
-
-	/* Atributos Primarios */
-	int str; // produce armor
-	int dex; // produce AtkSpd
-	int vit; // produce HP
-	String primaryAtt; // Atributo primario, depende de cada pj, otorga dmg
-
-	/* Atributos Secundarios o Derivados */
-	
-	int armor;
-
-	int lvl; // Nivel del personaje
-	int xp; // Experiencia acumulada
-
-	int maxHp; // HP total, el tope maximo
-	int currentHp; // HP actual, que va variando
-
-	int skillDmg; // dmg proveniente del skill utilizado
-	double atkSpd; // cantidad de ataques por segundo
+public interface FichaDePersonaje {
 
 	/**
-	 * CONSTRUCTOR. Metodo que inicializa la hoja de personaje. Se debe ejecutar
-	 * una vez durante la creacion del personaje. Le da al personaje un GEAR e
-	 * inicializa sus atributos primarios.
-	 * 
-	 */
-	public FichaDePersonaje() {
-		// Inicializao el Gear
-		this.gear = new Gear();
-
-		// Si se agregan clases de PJ
-		// Los atributos se deben iniciarlizar
-		// diferente para cada clase
-		// Y el PrimaryAtt depende de cada clase
-		setStr(10);
-		setDex(8);
-		setVit(9);
-		setPrimaryAtt("str");
-
-		setMaxHp();
-		setCurrentHp(getMaxHp());
-
-	}
-
-	/**
-	 * Devuelve el valor del Primary Attribute.
-	 * 
-	 * @return
-	 */
-	private int getPrimaryAttValue() {
-		if (getPrimaryAtt() == "str") {
-			return getStr();
-		}
-		if (getPrimaryAtt() == "dex") {
-			return getDex();
-		}
-		else{
-			return 0;
-		}
-	}
-
-	/* FORMULAS */
-	/*
-	 * Formula Total Armor: strArmor + Armor
-	 * 
-	 * Se debe recalcular cada vez que se cambia de equipo
-	 * O el atributo STR cambia
-	 * 
-	 */
-	private void calcTotalArmor() {
-		int totalArmor = 0;
-		totalArmor += getGear().getTotalGearArmor(); // Armor del equipo.
-		totalArmor += getStr(); // Sumado al armor 1:1 de la Str.
-
-		setArmor(totalArmor);
-	}
-
-	/**
-	 * Damage Reduction Formula: (TotalArmor / ( 50 * Attacker Level +
-	 * TotalArmor))*100
-	 * 
-	 * @return el % de reduccion de dmg.
-	 */
-	private int getDamageReduction(int attackerLevel) {
-		int dmgRed = 0;
-		int numerador = getArmor();
-		int denominador = 50 * attackerLevel;
-		denominador += getArmor();
-		denominador *= 100;
-		dmgRed = numerador / denominador;
-
-		return dmgRed;
-	}
-
-	/**
-	 * SkillDamage Formula: WpnDmg*x% (donde x es el porcentaje de dmg del arma
-	 * que hace el skill... pe: 110%, 150%, 200%, etc..)
-	 * 
-	 * @param wpnDmgPercent
-	 *            porcentaje (en base al wpnDmg) de dmg del skill
-	 */
-	private void setSkillDamage(int wpnDmgPercent) {
-		int wpnDmg = getGear().getWpnDmg();
-		setSkillDmg((wpnDmgPercent * wpnDmg) / 100);
-	}
-
-	/**
-	 * DAMAGE Formula: ( WpnDmg * SkillDmg ) * ( 1 + (PrimartySt / 100) ) ANTES
-	 * DE LLAMAR a getDmg() se debe llamar a setSkillDamage(int wpnDmgPercent)
-	 * 
-	 * @return
-	 */
-	private int getDmg() {
-		int skillDmg = getSkillDmg();
-		int wpnDmg = getGear().getWpnDmg();
-		int damage = 0;
-		damage = skillDmg * wpnDmg;
-		damage *= (1 + (getPrimaryAttValue() / 100));
-
-		return damage;
-	}
-
-	/**
-	 * FORMULA DexAtkSpd: Dex * 0.1% FORMULA AtkSpd: WpnAtkSpd * ( 1 + DexAtkSpd
-	 * )
-	 * 
-	 * @return
-	 */
-	private double getAndSetAtkSpd() {
-		double wpnAtkSpd = gear.getWpnAtkSpd();
-		double dexAtkSpd = (0.1 * getDex()) / 100;
-
-		setAtkSpd(wpnAtkSpd * (1 + dexAtkSpd));
-		return round((wpnAtkSpd * (1 + dexAtkSpd)), 2);
-	}
-
-	/**
-	 * 
-	 * Formula: Dmg - (Dmg*DmgRed%)
+	 * Le indica a la FichaDePersonaje que fue atacado y recibio dmg, la ficha
+	 * sabe hacer los calculos y restar la vida correspondiente.
 	 * 
 	 * @param dmg
-	 *            --> dmg es pasado por el jugador atacante
-	 * @return el dmg real que es restado a la HP.
+	 *            recibido en un ataque.
+	 * @param attackerLvl
+	 *            nivel del actor atacante.
 	 */
-	private int getDamageTaken(int dmg, int attackerLvl) {
-		int dmgRed = getDamageReduction(attackerLvl);
-		int dmgTaken = dmg - ((dmgRed * dmg) / 100);
-		int hp = getCurrentHp();
-		hp -= dmgTaken;
-		setCurrentHp(hp);
-		return dmgTaken;
-	}
+	void recibiDmg(int dmg, int attackerLvl);
 
 	/**
+	 * Le da al actor su total de dmg al atacar. Para poder golpear a otro actor
 	 * 
-	 * Formula Hit Points: 36 + (4*LVL) + (10 * VIT)
+	 * @return total de dmg que hace el Pj.
 	 */
-	private void setMaxHp() {
-		int maxHp;
-		maxHp = 36 + (4 * getLvl() + (10 + getVit()));
-		this.maxHp = maxHp;
-	}
-
-	/* UTILS */
+	int dameDmg();
 
 	/**
-	 * Redondea un numero del tipo double, acortando los decimales
+	 * El Attack Speed del personaje. Es un punto flotante con 1 decimal, que
+	 * representa los ataques por segundo. p.e: 1,8 ataques por segundo.
 	 * 
-	 * @param numero
-	 *            que se quiere redondear
-	 * @param decimales
-	 *            cantidad de decimales
-	 * @return el numero ya redondeado.
+	 * @return el AtkSpd.
 	 */
-	private double round(double numero, int decimales) {
-		return Math.round(numero * Math.pow(10, decimales))
-				/ Math.pow(10, decimales);
-	}
+	double dameAtkSpd();
 
-	/* GETTERS Y SETTERS */
-	private int getStr() {
-		return str;
-	}
+	/**
+	 * Le pide a la HojaDePersonaje el HP actual del pj
+	 * 
+	 * @return currentHP
+	 */
+	int dameCurrentHp();
 
-	private void setStr(int str) {
-		this.str = str;
-	}
+	/**
+	 * Le pide a la Hoja de Personaje el HP maximo.
+	 * 
+	 * @return MaxHp
+	 */
+	int dameMaxHp();
 
-	private int getDex() {
-		return dex;
-	}
+	/**
+	 * Le pide a la HojaDePersonaje el nivel del PJ. Sirve para pasarselo junto
+	 * con el DMG al realizar un ataque.
+	 * 
+	 * @return nivel de experiencia de un PJ.
+	 */
+	int dameLvl();
 
-	private void setDex(int dex) {
-		this.dex = dex;
-	}
-
-	private int getVit() {
-		return vit;
-	}
-
-	private void setVit(int vit) {
-		this.vit = vit;
-	}
-
-	private String getPrimaryAtt() {
-		return primaryAtt;
-	}
-
-	private void setPrimaryAtt(String primaryAtt) {
-		this.primaryAtt = primaryAtt;
-	}
-
-	private Gear getGear() {
-		return gear;
-	}
-
-	private void setGear(Gear gear) {
-		this.gear = gear;
-	}
-
-	private int getSkillDmg() {
-		return skillDmg;
-	}
-
-	private void setSkillDmg(int skillDmg) {
-		this.skillDmg = skillDmg;
-	}
-
-	private double getAtkSpd() {
-		return atkSpd;
-	}
-
-	private void setAtkSpd(double atkSpd) {
-		this.atkSpd = atkSpd;
-	}
-
-	private int getLvl() {
-		return lvl;
-	}
-
-	private void setLvl(int lvl) {
-		this.lvl = lvl;
-	}
-
-	private int getCurrentHp() {
-		return currentHp;
-	}
-
-	private void setCurrentHp(int currentHp) {
-		this.currentHp = currentHp;
-	}
-
-	private int getMaxHp() {
-		return maxHp;
-	}
-
-	private int getArmor() {
-		return armor;
-	}
-
-	private void setArmor(int armor) {
-		this.armor = armor;
-	}
+	/**
+	 * El total de experiencia. La cantidad de xp determina el Lvl del pj.
+	 * 
+	 * @return total de Xp.
+	 */
+	int dameXp();
 }
