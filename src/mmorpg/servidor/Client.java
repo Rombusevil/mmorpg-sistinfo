9067a56f9869dde8ada58c564e4dbb24a583df07
franco.mgr <franco.mgr@0634dfc2-1731-0b68-b3c3-2be22b4b793a>
2012-11-24 23:31:41 +0000

    Commit menor2

--- src/mmorpg/servidor/Client.java
+++ src/mmorpg/servidor/Client.java
@@ -1,10 +1,17 @@
 package mmorpg.servidor;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 public class Client {
 	
-	DecodificadorTeclas decoderTeclas;
+	private DecodificadorTeclas decoderTeclas;
+	
+	private ObjectOutputStream output; 	// En esta variable se guardan los mensajes para enviar
+	private ObjectInputStream input; 	// En esta variable se reciben mensajes desde afuera
 	
 	public Client(){
+		
 		
 	}
 }
