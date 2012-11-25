5132c5380376a53e342a83cd31aaa6a4ac8e064e
franco.mgr <franco.mgr@0634dfc2-1731-0b68-b3c3-2be22b4b793a>
2012-11-25 01:12:03 +0000

    Commit menor2

--- src/mmorpg/servidor/GestorComandos.java
+++ src/mmorpg/servidor/GestorComandos.java
@@ -88,28 +88,14 @@ public class GestorComandos implements Runnable{
 
 
 
-		// Configura el stream para enviar y recibir mensajes
-		private void setupStreams() throws IOException{
-			output = new ObjectOutputStream(connection.getOutputStream()); // la conexion establecida previamente SOCKET CONECTADO -> GestorComandos
-			output.flush(); // Vacia la "basura" del buffer
-			
-			input = new ObjectInputStream(connection.getInputStream()); // Lo mismo pero para el inputStream. Aca recibo informacion.
-			//gui.mostrarMensaje("\n Streams configurados \n");		
-		}
-		
-		
-		// Durante la conexion establecida.. se envian y reciben mensajes
-		private void whileRunning() throws IOException{
-			String msg = "Ahora estas conectado.";
-			//gui.mostrarMensaje(msg);
-			
-			do{
-				try{
-					msg = (String) input.readObject(); //Leo el input y lo casteo a String ---> Castear a "Comando" o a "Ejecutable" para el GestorComandos
-					//gui.mostrarMensaje("\n"+msg);
-				}catch(ClassNotFoundException ex){
-					//gui.mostrarMensaje("\n Comando incorrecto! Mensaje no entendido \n");
-				}
-			}while(true); // Mientras stop == false
-		}
+//	// Configura el stream para enviar y recibir mensajes
+//	private void setupStreams() throws IOException{
+//		output = new ObjectOutputStream(connection.getOutputStream()); // la conexion establecida previamente SOCKET CONECTADO -> GestorComandos
+//		output.flush(); // Vacia la "basura" del buffer
+//		
+//		input = new ObjectInputStream(connection.getInputStream()); // Lo mismo pero para el inputStream. Aca recibo informacion.
+//		//gui.mostrarMensaje("\n Streams configurados \n");		
+//	}
+//	
+
 }
