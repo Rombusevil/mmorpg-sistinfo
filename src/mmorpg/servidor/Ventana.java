cdddae19c9e466c5c19481f4d515241b58d2d9ec
iberparodisiri@gmail.com <iberparodisiri@gmail.com@0634dfc2-1731-0b68-b3c3-2be22b4b793a>
2012-11-28 23:29:45 +0000

    reziseado

--- src/mmorpg/servidor/Ventana.java
+++ src/mmorpg/servidor/Ventana.java
@@ -37,7 +37,7 @@ public class Ventana extends JFrame implements KeyListener{
 		j1.setFont(new Font("Courier New", Font.PLAIN, 12));
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		add(j1,BorderLayout.NORTH);
-		setSize(550,120);
+		setSize(800,800);
 		setVisible(true);
 		setFocusable(true);
 	}
